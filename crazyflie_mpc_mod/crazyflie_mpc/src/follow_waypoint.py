23a23b28c1db1988daf025b24d624747d9a445cd
ZHIYANG CHEN <54970284+SamoaChen@users.noreply.github.com>
2022-02-13 20:28:45 -0500

    Add files via upload

--- crazyflie_mpc_mod/crazyflie_mpc/src/follow_waypoint.py
+++ crazyflie_mpc_mod/crazyflie_mpc/src/follow_waypoint.py
@@ -27,7 +27,7 @@ class MPCDemo():
         # self.m_serviceTakeoff = rospy.Service('takeoff', , self.takeoffService)
         # frames and transforms
         self.worldFrame = rospy.get_param("~world_frame", "world")
-        quad_name = "crazyflie_mpc"
+        quad_name = "crazy_mpc"
         self.frame = quad_name
         #self.frame = rospy.get_param("~frame")
         self.tf_listener = TransformListener()
@@ -61,18 +61,14 @@ class MPCDemo():
         y_traj = radius * np.sin(t_plot) + 4.5455
   
         z_traj = np.zeros((len(t_plot),)) + 0.4
-        points = np.stack((x_traj, y_traj, z_traj), axis=1)
-        print("points shape", points.shape)
+        #points = np.stack((x_traj, y_traj, z_traj), axis=1)
+        #print("points shape", points.shape)
         #points[-1, 2] = 0.2 #final landing height - > convert to a rosservice
-        print("final points shape", points.shape)
-        #points = np.array([  # points for generating trajectory
-        #                   [-1.409, 2.826, 0.55],
-        #                   [1.609, 2.826, 0.55],
-        #                   [1.609, 5.826, 0.55],
-        #                   [-1.409, 5.826, 0.55],
-        #                   [-1.409, 2.826, 0.55],
-        #                   [-1.409, 2.826, 0.3],
-        #                   [-1.409, 2.826, 0.0]])
+        #print("final points shape", points.shape)
+        points = np.array([  # points for generating trajectory
+                           [-0.3, -1.5, 0.1],
+                           [-0.3, -1.5, 0.2],
+                           [-0.3, -1.5, 0.1]])
         #points = np.array([[0.,-2.,0.],
         #                   [0.,-2.,0.4],
         #                   [0.,-2.,0.]])
